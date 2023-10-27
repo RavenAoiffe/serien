a4ce71bb2e0fb6c3cef0a941285b87ccc5ebc416
Ameyalli <ameyanezcmm@gmail.com>
2023-10-27 14:00:07 -0600

    Export functions for testing

--- src/components/Calculate.jsx
+++ src/components/Calculate.jsx
@@ -1,33 +1,33 @@
 import Result from './Result';
 
-export function sum(a,b){
-    return a + b
-}
-
-export function fibonacci(n){
+export function fibonacci(n) {
     let a = 0, b = 1, c = n;
-  
-    for(let i = 2; i <= n; i++) {
-      c = a + b;
-      a = b;
-      b = c;
+
+    for (let i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
     }
     return c;
-  }
-  export function triangular(n){
-    return(n*(n+1)/2);
-  }
- 
-const Calculate = ({numero}) => {
-      
-      const primo = n =>{
-        let raiz = Math.sqrt(n)
-        let log = Math.log(n) * Math.LOG10E;
-        return (raiz/log-1)
-      }
+}
+export function triangular(n) {
+    return (n * (n + 1) / 2);
+}
 
-      let resultado = (4*primo(numero) - triangular(numero))/fibonacci(numero + 2)
 
+export function primo(n) {
+    let raiz = Math.sqrt(n)
+    let log = Math.log(n) * Math.LOG10E;
+    return (raiz / log - 1)
+}
+
+export function serieN(numero) {
+    return (4 * primo(numero) - triangular(numero)) / fibonacci(numero + 2)
+}
+
+const Calculate = ({ numero }) => {
+
+    let resultado = serieN(numero)
     return (
         <Result
             resultado={resultado}
--- src/test/Calculate.test.js
+++ src/test/Calculate.test.js
@@ -1,9 +1,5 @@
 import { expect, test } from 'vitest'
-import {sum, fibonacci} from '../components/Calculate'
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3)
-})
+import { fibonacci} from '../components/Calculate'
 
 test('fibonnaci for number 3 equal to 2', () => {
     expect(fibonacci(3)).toBe(2)
