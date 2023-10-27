440d632fda61602c06dc0c5bcba55196eb38a518
Ameyalli <ameyanezcmm@gmail.com>
2023-10-26 18:36:44 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Vite + React</h1>
+      <div className="card">
+        <p>Sngular prueba</p>
+      </div>
+    </>
+  )
+}
+
+export default App
